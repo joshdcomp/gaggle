4666e29eec3b37cab346802806b0c61bc2703d40
Josh Compton <josh.compton@attck.com>
2016-03-09 01:14:44 -0500

    Adds optional params to routes

--- assets/in/js/routes.jsx
+++ assets/in/js/routes.jsx
@@ -4,7 +4,7 @@ var ReactRouter = require("react-router");
 var Router     = ReactRouter.Router;
 var Route      = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
-var BrowserHistory = ReactRouter.browserHistory;
+var browserHistory = ReactRouter.browserHistory;
 
 var App = require('./routes/app.jsx');
 
@@ -14,11 +14,11 @@ var ViewLookup = require('./routes/view-lookup.jsx');
 var ViewSubmit = require('./routes/view-submit.jsx');
 
 var GaggleRoutes = (
-  <Router history={BrowserHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={ViewLearn} />
-      <Route name="RouteLearn" path="/learn" component={ViewLearn} />
-      <Route name="RouteLookup" path="/lookup" component={ViewLookup} />
+      <Route name="RouteLearn" path="/learn(/:animal(/:pic))" component={ViewLearn} />
+      <Route name="RouteLookup" path="/lookup(/:type/:term)" component={ViewLookup} />
       <Route name="RouteSubmit" path="/submit" component={ViewSubmit} />
     </Route>
   </Router>
